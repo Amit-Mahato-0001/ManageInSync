968ab50cc9d3a4de148ebe112fbe7da39d3c3c27
dellson-arch <nayanmahato908@gmail.com>
2026-01-18 10:32:15 +0530

    token removed from signin and login

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,62 @@
+//input se agencyName, email, password lo
+//ager input missing he toh error show
+// signup krne bolo signup service ko
+//success response
+//error
+
+const {signup, login} = require('../services/auth.service')
+
+const signupHandler = async (req, res) => {
+
+    try {
+        const {agencyName, email, password} = req.body
+
+        if(!agencyName || !email || !password){
+            return res.status(400).json({
+                message: "Please fill all required fields"
+            })
+        }
+
+        const result = await signup({ agencyName, email, password})
+
+        return res.status(201).json({
+            message: "Signup successful",
+            // token: result.token //we never show the token here for safety purpose if token is shared then ultimately privacy is compromised
+            data : result
+        })
+
+    } catch (error) {
+        return res.status(400).json({
+            message: error.message
+        })
+    }
+}
+
+const loginHandler = async (req, res) => {
+
+    try {
+        const {email, password} = req.body
+
+        if(!email || !password){
+            return res.status(400).json({
+                message: "Invalid credentials"
+            })
+        }
+
+        const result = await login({email, password})
+
+        return res.status(201).json({
+            message: "Login successful",
+            // token: result.token
+            result
+        })
+
+    } catch (error) {
+        return res.status(400).json({
+            message: error.message
+        })
+    }
+}
+
+
+module.exports = {signupHandler, loginHandler}
